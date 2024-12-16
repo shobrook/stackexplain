29010c7c5ab555bf9e6cd008d8e5b6843ecc27b4
Rob van Gastel <RobvanGastel@users.noreply.github.com>
2024-12-16 08:23:24 +0100

    Bump the wut-cli version to 1.0.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info[:3] < (3, 0, 0):
 
 setup(
     name="wut-cli",
-    version="1.0.4",
+    version="1.0.5",
     description="CLI that explains the output of your last command",
     url="https://github.com/shobrook/wut",
     author="shobrook",
